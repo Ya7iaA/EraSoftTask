f701bd7253261e1b62d8c4200362127906234489
Ya7iaA <yahiaahmed.y72a@gmail.com>
2025-08-27 20:14:05 +0300

    Database task done

---
+++ ProblemSolvingDatabase/FirstLec.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Manger (
+    id NUMBER PRIMARY KEY,
+    name VARCHAR2(100),
+    age NUMBER,
+    birth_date DATE,
+    address VARCHAR2(200)
+);
+
+ALTER TABLE Manger DROP COLUMN address;
+ALTER TABLE Manger ADD (
+    city_address VARCHAR2(100),
+    street VARCHAR2(100)
+);
+
+ALTER TABLE Manger RENAME COLUMN name TO full_name;
+
+ALTER TABLE Manger READ ONLY;
+
+CREATE TABLE Owner AS 
+SELECT id, full_name AS name, birth_date 
+FROM Manger 
+WHERE 1 = 0;
+
+ALTER TABLE Manger RENAME TO Master;
+
+ALTER TABLE Master READ WRITE;
+ALTER TABLE Owner READ WRITE;
+DROP TABLE Master CASCADE CONSTRAINTS;
+DROP TABLE Owner CASCADE CONSTRAINTS;
